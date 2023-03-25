9610cf2f1f1def7e72eb793b4d44cf6e9d79c630
kovsi <kovsiun@gmail.com>
2023-03-25 17:17:37 +0100

    Initial commit

---
+++ src/main/java/com/ivankauchuha/codesharingplatformproject/CodeSharingPlatformProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ivankauchuha.codesharingplatformproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSharingPlatformProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSharingPlatformProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivankauchuha/codesharingplatformproject/CodeSharingPlatformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivankauchuha.codesharingplatformproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSharingPlatformProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
